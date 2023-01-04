9e0ccd8e139533574708877a7a1bfcf7420d78f4
Natalia Moskvychova <110030591+NaTaLiaMoSKV@users.noreply.github.com>
2023-01-04 10:36:09 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId;
 
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick(e) {
+    e.currentTarget.disabled = true;
+    stopBtn.disabled = false;
+    timerId = setInterval( () => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+}
+
+function onStopBtnClick(e) {
+    startBtn.disabled = false;
+    e.currentTarget.disabled = true;
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
